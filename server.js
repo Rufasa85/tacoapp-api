71f970a37fc3705583a5d85700db22adf6b71830
Joseph Rehfuss <josephrehfuss@D-10-156-9-244.dhcp4.washington.edu>
2019-08-05 11:08:52 -0700

    adding mock data;

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+// var allRoutes = require('./controllers');
+
+// // Requiring our models for syncing
+// var db = require('./models');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+// app.use(express.static('public'));
+
+// var exphbs = require('express-handlebars');
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+
+const tacos = [{
+    id:1,
+    filling:'fish',
+    isEaten:false
+},{
+    id:2,
+    filling:'beef',
+    isEaten:false
+},{
+    id:3,
+    filling:'choco',
+    isEaten:false
+}]
+
+// app.use('/',allRoutes);
+app.get('/',(req,res)=>{
+    res.json('Hello World')
+})
+
+app.get('/tacos',(req,res)=>{
+    res.json(tacos)
+})
+
+app.get('/tacos/:id',(req,res)=>{
+    res.json(tacos[req.params.id-1])
+})
+
+// db.sequelize.sync({ force: true }).then(function() {
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+});
+// });
